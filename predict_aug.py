6982fd7a3fd7d79cef6778c0458284ddfd354ef6
sashank <stsashank6@gmail.com>
2023-10-05 11:42:12 -0700

    Visualizations for val dataset made

---
+++ predict_aug.py
@@ -0,0 +1,304 @@
+import argparse
+import logging
+import os
+
+import albumentations as A
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms
+
+from unet import UNet
+from utils.data_loading import BasicDataset
+from utils.dice_score import dice_coeff, dice_loss, multiclass_dice_coeff
+from utils.utils import plot_img_and_maskv2 as plot_img_and_mask
+
+
+def add_snow_img(img):
+    return A.functional.add_snow(img, snow_point=0.1, brightness_coeff=2.5)
+
+
+def add_fog_img(img):
+    return A.functional.add_fog(img, fog_coef=0.4, alpha_coef=0.08, haze_list=())
+
+
+def add_rain_img(img):
+    height, width = img.shape[:2]
+    area = height * width
+    num_drops = area // 770
+    slant = 6
+    rain_drops = []
+    drop_length = 30
+
+    for _i in range(num_drops):  # If You want heavy rain, try increasing this
+        if slant < 0:
+            x = np.random.randint(slant, width)
+        else:
+            x = np.random.randint(0, width - slant)
+
+        y = np.random.randint(0, height - drop_length)
+
+        rain_drops.append((x, y))
+    return A.functional.add_rain(
+        img,
+        slant=slant,
+        drop_length=drop_length,
+        drop_width=1,
+        drop_color=(200, 200, 200),
+        blur_value=5,
+        brightness_coefficient=0.6,
+        rain_drops=rain_drops,
+    )
+
+
+def add_sun_flare_img(img):
+    height, width = img.shape[:2]
+    flare_center_x = int(0.7 * width)
+    flare_center_y = int(0.2 * height)
+    angle = 0.7
+    num_circles = 8
+    circles = []
+
+    x = []
+    y = []
+
+    def line(t):
+        return (
+            flare_center_x + t * np.math.cos(angle),
+            flare_center_y + t * np.math.sin(angle),
+        )
+
+    for t_val in range(-flare_center_x, width - flare_center_x, 10):
+        rand_x, rand_y = line(t_val)
+        x.append(rand_x)
+        y.append(rand_y)
+
+    for _i in range(num_circles):
+        alpha = np.random.uniform(0.05, 0.2)
+        r = np.random.randint(0, len(x) - 1)
+        rad = np.random.randint(1, max(height // 100 - 2, 2))
+
+        r_color = np.random.randint(max(255 - 50, 0), 255)
+        g_color = np.random.randint(max(255 - 50, 0), 255)
+        b_color = np.random.randint(max(255 - 50, 0), 255)
+
+        circles += [
+            (
+                alpha,
+                (int(x[r]), int(y[r])),
+                pow(rad, 3),
+                (r_color, g_color, b_color),
+            )
+        ]
+    return A.functional.add_sun_flare(
+        img,
+        flare_center_x,
+        flare_center_y,
+        400,
+        (255, 255, 255),
+        circles,
+    )
+
+
+def add_brightness(img, brightness_coeff=1.5):
+    return A.functional.adjust_brightness_torchvision(img, brightness_coeff)
+
+
+def add_high_brightness(img, brightness_coeff=1.5):
+    return A.functional.adjust_brightness_torchvision(img, brightness_coeff)
+
+
+def add_low_brightness(img, brightness_coeff=0.5):
+    return A.functional.adjust_brightness_torchvision(img, brightness_coeff)
+
+
+def add_none(img):
+    return img
+
+
+def add_aug(img, aug):
+    if aug == "snow":
+        return add_snow_img(img)
+    elif aug == "fog":
+        return add_fog_img(img)
+    elif aug == "rain":
+        return add_rain_img(img)
+    elif aug == "sun_flare":
+        return add_sun_flare_img(img)
+    elif aug == "high_brightness":
+        return add_high_brightness(img)
+    elif aug == "low_brightness":
+        return add_low_brightness(img)
+    elif aug == "none":
+        return add_none(img)
+    else:
+        raise ValueError("Invalid augmentation")
+
+
+def prepare_for_net(img, device):
+    if img.ndim == 2:
+        img_new = img[np.newaxis, ...]
+    else:
+        img_new = img.transpose((2, 0, 1))
+    if img_new.max() > 1:
+        img_new = img_new / 255.0
+    img_new = torch.from_numpy(img_new)
+    img_new = img_new.unsqueeze(0)
+    return img_new.to(device=device, dtype=torch.float32)
+
+
+def predict_img(
+    net,
+    full_img,
+    device,
+    scale_factor=1,
+    out_threshold=0.5,
+    fixed_scale=512,
+    aug="none",
+    mask_e=None,
+):
+    net.eval()
+    img = BasicDataset.preprocess(
+        None, full_img, scale_factor, is_mask=False, fixed_scale=fixed_scale
+    )
+    img = add_aug(img, aug)
+    inp_img = img.copy()
+    img = prepare_for_net(img, device)
+    with torch.no_grad():
+        output = net(img).cpu()
+        if net.n_classes > 1:
+            mask = output.argmax(dim=1)
+        else:
+            mask = torch.sigmoid(output) > out_threshold
+    mask = mask[0].long().squeeze().numpy()
+    mask = mask[:, :].astype(np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((71, 71), np.uint8))
+    mask = mask * mask_e
+
+    return mask, inp_img
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Predict masks from input images")
+    parser.add_argument(
+        "--model",
+        "-m",
+        default="MODEL.pth",
+        metavar="FILE",
+        help="Specify the file in which the model is stored",
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        metavar="INPUT",
+        nargs="+",
+        help="Filenames of input images",
+        required=True,
+    )
+    parser.add_argument(
+        "--output", "-o", metavar="OUTPUT", nargs="+", help="Filenames of output images"
+    )
+    parser.add_argument(
+        "--viz",
+        "-v",
+        action="store_true",
+        help="Visualize the images as they are processed",
+    )
+    parser.add_argument(
+        "--no-save", "-n", action="store_true", help="Do not save the output masks"
+    )
+    parser.add_argument(
+        "--mask-threshold",
+        "-t",
+        type=float,
+        default=0.5,
+        help="Minimum probability value to consider a mask pixel white",
+    )
+    parser.add_argument(
+        "--scale",
+        "-s",
+        type=float,
+        default=0.5,
+        help="Scale factor for the input images",
+    )
+    parser.add_argument(
+        "--bilinear", action="store_true", default=False, help="Use bilinear upsampling"
+    )
+    parser.add_argument(
+        "--classes", "-c", type=int, default=2, help="Number of classes"
+    )
+
+    return parser.parse_args()
+
+
+def get_output_filenames(args):
+    def _generate_name(fn):
+        return f"{os.path.splitext(fn)[0]}_OUT.png"
+
+    return args.output or list(map(_generate_name, args.input))
+
+
+def mask_to_image(mask: np.ndarray, mask_values):
+    if isinstance(mask_values[0], list):
+        out = np.zeros(
+            (mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8
+        )
+    elif mask_values == [0, 1]:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=bool)
+    else:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=np.uint8)
+
+    if mask.ndim == 3:
+        mask = np.argmax(mask, axis=0)
+
+    for i, v in enumerate(mask_values):
+        out[mask == i] = v
+
+    return Image.fromarray(out)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+    in_files = args.input
+    out_files = get_output_filenames(args)
+
+    net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"Loading model {args.model}")
+    logging.info(f"Using device {device}")
+
+    net.to(device=device)
+    state_dict = torch.load(args.model, map_location=device)
+    mask_values = state_dict.pop("mask_values", [0, 1])
+    net.load_state_dict(state_dict)
+
+    logging.info("Model loaded!")
+
+    for i, filename in enumerate(in_files):
+        logging.info(f"Predicting image {filename} ...")
+        img = Image.open(filename)
+
+        mask = predict_img(
+            net=net,
+            full_img=img,
+            scale_factor=args.scale,
+            out_threshold=args.mask_threshold,
+            device=device,
+        )
+
+        if not args.no_save:
+            out_filename = out_files[i]
+            result = mask_to_image(mask, mask_values)
+            result.save(out_filename)
+            logging.info(f"Mask saved to {out_filename}")
+
+        if args.viz:
+            logging.info(
+                f"Visualizing results for image {filename}, close to continue..."
+            )
+            plot_img_and_mask(img, mask)
--- predict_video.py
+++ predict_video.py
@@ -10,7 +10,7 @@ import torch
 import tqdm
 from PIL import Image
 
-from predict import predict_img
+from predict_aug import predict_img
 from unet import UNet
 from utils.data_loading import BasicDataset
 from utils.utils import plot_img_and_maskv2 as plot_img_and_mask
@@ -27,6 +27,14 @@ def image_sort_key(image_name: str) -> int:
         int: timestamp
     """
     return int(os.path.splitext(image_name)[0][:-5])
+
+
+def calc_blade_load(mask):
+    empty = 136096
+    full = 52200
+    cur = np.count_nonzero(mask)
+    blade_load = (cur - empty) * 100 / (full - empty)
+    return blade_load
 
 
 if __name__ == "__main__":
@@ -73,15 +81,169 @@ if __name__ == "__main__":
     output_dir.mkdir(exist_ok=True)
     output_pth = output_dir / (Path(args.input_folder).name + ".mp4")
     writer = cv2.VideoWriter(
-        str(output_pth), cv2.VideoWriter_fourcc(*"mp4v"), 10, (1024, 512)
+        str(output_pth), cv2.VideoWriter_fourcc(*"mp4v"), 10, (1200, 800)
     )
     i = 0
+    mask_e = Image.open("mask_empty.png").resize((512, 512))
+    mask_e = np.asarray(mask_e) / 255  # 136096
+    mask_f = Image.open("mask_full.png").resize((512, 512))
+    mask_f = np.asarray(mask_f) / 255  # 52200
+    augmentations = [
+        "snow",
+        "rain",
+        "fog",
+        "sun_flare",
+        "high_brightness",
+        "low_brightness",
+    ]
+    blade_load_preds = {}
+    blade_load_preds["none"] = []
+    for aug in augmentations:
+        blade_load_preds[aug] = []
+    count = 0
     for image_file in tqdm.tqdm(image_files):
         img = Image.open(Path(args.input_folder) / image_file)
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+        i = 0
         mask, inp_img = predict_img(
-            net=net, full_img=img, scale_factor=0.5, out_threshold=0.5, device=device
+            net=net,
+            full_img=img,
+            scale_factor=0.5,
+            out_threshold=0.5,
+            device=device,
+            mask_e=mask_e,
+            aug="none",
         )
-        img = plot_img_and_mask(inp_img, mask, returns_img=True)
-        i += 1
+        blade_load = calc_blade_load(mask)
+        if count > 10:
+            blade_load_preds["none"].append(blade_load)
+        fig.suptitle(f"original blade_load: {int(blade_load)}")
+        for row in range(2):
+            for col in range(3):
+                aug = augmentations[i]
+
+                mask, inp_img = predict_img(
+                    net=net,
+                    full_img=img,
+                    scale_factor=0.5,
+                    out_threshold=0.5,
+                    device=device,
+                    mask_e=mask_e,
+                    aug=aug,
+                )
+
+                blade_load = calc_blade_load(mask)
+                axs[row - 1, col].set_title(f"{aug}:{int(blade_load)}")
+                out_img = plot_img_and_mask(inp_img, mask, returns_img=True)
+                out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+                axs[row - 1, col].imshow(
+                    out_img,
+                )
+                if count > 10:
+                    blade_load_preds[aug].append(blade_load)
+
+                i += 1
+
+        plt.tight_layout()
+        plt.savefig("/tmp/fig.png")
+        plt.close()
+        img = cv2.imread("/tmp/fig.png")
         writer.write(img)
+        count += 1
+        if count == 10:
+            # break
+            pass
+    #     for aug in augmentations:
+    #         mask, inp_img = predict_img(
+    #         net=net,
+    #         full_img=img,
+    #         scale_factor=0.5,
+    #         out_threshold=0.5,
+    #         device=device,
+    #         mask_e=mask_e,
+    #         aug="snow",
+    #         )
+    #         img = plot_img_and_mask(inp_img, mask, returns_img=True)
+
+    #     i += 1
+    #     writer.write(img)
     writer.release()
+    blade_load_err = {}
+    max_load_error = {}
+    for aug in augmentations:
+        blade_load_preds[aug] = np.asarray(blade_load_preds[aug])
+        avg_error = np.mean(np.abs(blade_load_preds[aug] - blade_load_preds["none"]))
+        max_error = np.std(np.abs(blade_load_preds[aug] - blade_load_preds["none"]))
+        blade_load_err[aug] = avg_error
+        max_load_error[aug] = max_error
+
+    # Sort keys to have 'none' at the left-most position
+    sorted_keys = np.array([k for k in blade_load_err.keys() if k != "none"])
+
+    # Get values in the same sorted order
+    sorted_values = np.array([blade_load_err[k] for k in sorted_keys])
+    sorted_max_error = np.array([max_load_error[k] for k in sorted_keys])
+
+    import seaborn as sns
+
+    # Seaborn style
+    sns.set(style="whitegrid")
+
+    # Custom colors
+    colors = ["#FFCCCB", "#FFDAB9", "#E6E6FA", "#D8BFD8", "#B0E0E6", "#98FB98"]
+
+    # Initialize the plot
+
+    # Create the bar chart
+    # barplot = sns.barplot(x=sorted_keys, y=sorted_values, palette=colors)
+    # breakpoint()
+    print(f"sorted_values: {sorted_values}, shape: {len(sorted_values)}")
+    print(f"sorted_max_error: {sorted_max_error}, shape: {len(sorted_max_error)}")
+    print(f"sorted_keys: {sorted_keys}, shape: {len(sorted_keys)}")
+    # barplot = sns.barplot(
+    #     x=sorted_keys,
+    #     y=sorted_values,
+    #     palette=colors,
+    #     yerr=sorted_max_error,
+    #     capsize=0.2,
+    # )
+    # # ax = sns.barplot(x="Augmentation", y="Average Error", data=df, estimator=np.mean, ci=85, capsize=.2, color='lightblue')
+
+    # # Add labels and title
+    # # plt.xlabel("Augmentation", fontsize=14)
+    # # plt.ylabel("Average Error", fontsize=14)
+    # plt.title("Average Error by Augmentation Type", fontsize=16)
+
+    # # Annotate bars with the actual values
+    # for p in barplot.patches:
+    #     barplot.annotate(
+    #         f"{p.get_height()}",
+    # #         (p.get_x() + p.get_width() / 2.0, p.get_height()),
+    #         ha="center",
+    #         va="baseline",
+    #         fontsize=12,
+    #         color="black",
+    #         xytext=(0, 5),
+    #         textcoords="offset points",
+    #     )
+
+    # Remove the top and right spines for better aesthetics
+    # sns.despine()
+    # Create the bar chart
+    fig, ax = plt.subplots()
+    bars = ax.bar(
+        sorted_keys,
+        sorted_values,
+        color=["#FFCCCB", "#FFDAB9", "#E6E6FA", "#D8BFD8", "#B0E0E6", "#98FB98"],
+        yerr=sorted_max_error,
+        capsize=5,
+    )
+
+    # Add labels and title
+    ax.set_xlabel("Augmentation")
+    ax.set_ylabel("Average Error")
+    ax.set_title("Average Error by Augmentation Type")
+
+    # Show plot
+    plt.savefig("avg_error.png")
+    breakpoint()
